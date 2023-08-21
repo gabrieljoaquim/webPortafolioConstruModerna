1539141411f29ba000e4eb6aea60e39ee62f98b2
gabrieljoaquim <70859332+gabrieljoaquim@users.noreply.github.com>
2023-08-21 18:45:42 -0500

    se hicieron camios en la precentacion del menu y del portafolo

---
+++ src/components/carrouselNavbar.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="container-fluid">
+    <div class="carrousel">
+        <div class="elemento">
+            <img  src="@/assets/img/CONSTRUCCION.jpg" alt="">
+        </div>
+        <div class="elemento">
+            <router-link to="/pinturas" class="mr-3">
+                <img  src="@/assets/img/cerrajeria.jpg" alt="">
+            </router-link>
+        </div>
+        <div class="elemento">
+            <img  src="@/assets/img/plomeria.jpg" alt="">
+        </div>
+        <div class="elemento">
+            <img  src="@/assets/img/pisosPVC.jpg" alt="">
+        </div>
+        <div class="elemento">
+            <img  src="@/assets/img/macrisal-practicable.jpg" alt="">
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name:'carrouseMenu'
+}
+</script>
+
+<style scoped>
+
+.carrousel {
+    margin-top: 5%;
+    padding: 5px;
+    display: flex;
+    flex-wrap: nowrap;
+    overflow: scroll;
+}
+
+.elemento {
+    flex: 1 0 auto;
+    margin: 0 10px;
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.3);
+}
+.elemento img {
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+    border: 2px solid white;
+}
+
+</style>
---
+++ src/views/registro.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="container-fluid">
+    <div class="seccion">
+        <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Apellido" aria-label="Apellido" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Correo" aria-label="Correo" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Clave" aria-label="Clave" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Confirme Clave" aria-label="Confrime Clave" aria-describedby="basic-addon1">
+    </div>
+    <div class="mb-3 form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+            Asepto terminos.
+        </label>
+    </div>
+    <div class="input-group mb-3 d-grid gap-2">
+        <button type="button" class="btn btn-primary btn-sm">Small button</button>
+    </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+name: 'registro',
+
+}
+</script>
+
+<style scoped>
+
+    .container-fluid{
+        height: 100vh;
+    }
+    .seccion{
+        height: 400px;
+        padding-top: 100px;
+    }
+</style>
